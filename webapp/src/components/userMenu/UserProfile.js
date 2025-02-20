ee056e5024373443d7dcc3ab2347298432101930
Claudia <uo294420@uniovi.es>
2025-02-20 21:05:04 +0100

    Creación del Componente UserProfile

---
+++ webapp/src/components/userMenu/UserProfile.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+
+export const UserPofile = () => {
+    
+
+}
