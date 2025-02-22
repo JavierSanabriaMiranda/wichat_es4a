f05083ecdc4a82bd2d2af3d5fff0d3d51c4d6063
Claudia <uo294420@uniovi.es>
2025-02-22 14:56:52 +0100

    Cambio de Componente Renderizado

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserPofile } from './components/userMenu/UserProfile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App /> */}
+    <UserPofile userName={"uo294420"} onSelectOption={"edit"}/>
   </React.StrictMode>
 );
 
