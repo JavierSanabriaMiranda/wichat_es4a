fb453351099bd267a11e0c2af8da1aeae5632c24
Claudia <uo294420@uniovi.es>
2025-03-03 19:37:16 +0100

    Añadido Número de Pregunta

--- webapp/src/components/gameHistory/QuestionAccordion.js
+++ webapp/src/components/gameHistory/QuestionAccordion.js
@@ -10,12 +10,12 @@ import Image from 'react-bootstrap/Image';
  * This component represents each of the questions displayed in the history or summary of a game.
  * Each question has a topic, an image, and a list of answers.
  */
-export const QuestionAccordion = ({ questions }) => {
+export const QuestionAccordion = ({ questions}) => {
     return (
         <Accordion>
             {questions.map((question, index) => (
                 <Accordion.Item eventKey={index.toString()} key={index}>
-                    <Accordion.Header>{question.topic}</Accordion.Header>
+                    <Accordion.Header>{index+1}. {question.topic}</Accordion.Header>
                     <Accordion.Body>
                         <Image 
                             src={question.imageUrl} 
