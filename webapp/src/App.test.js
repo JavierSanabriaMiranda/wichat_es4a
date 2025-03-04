dc4000a8fb16a35e790088e9c5a398bdc0ecaa65
Adriana Herrero González <UO287543@uniovi.es>
2025-03-04 13:52:20 +0100

    cambiar mensaje esperado en el test

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders welcome message', () => {
   render(<App />);
-  const welcomeMessage = screen.getByText(/Welcome to the 2025 edition of the Software Architecture course/i);
+  const welcomeMessage = screen.getByText(/Bienvenido a WiChat/i);
   expect(welcomeMessage).toBeInTheDocument();
 });
 
