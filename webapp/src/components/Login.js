d0ee6f835cf388ff35bf645167cb300c1dea23d2
Aitorsiius <aitorcius2004@gmail.com>
2025-03-07 22:07:30 +0100

    Fix LLM para pistas

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
 import { Typewriter } from "react-simple-typewriter";
-import getClue from '../LLM';
 
 const Login = () => {
   const [username, setUsername] = useState('');
