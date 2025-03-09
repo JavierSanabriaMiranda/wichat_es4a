6f191a69aa84a2bb084c19926ec42ec63697ebaa
uo293758 <uo293758@uniovi.es>
2025-03-09 01:45:02 +0100

    Mejora de bloqueo de botones en carga de pregunta + animaciones de carga
    Se han implementado mejoras en el momento de carga de una pregunta, bloqueando los botones de respuesta y mostrando una animación de carga.

--- webapp/src/components/game/Timer.js
+++ webapp/src/components/game/Timer.js
@@ -1,12 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 /**
  * React component to represent a timer whose time is decremented every second.
  * @param {Number} initialTime - The initial time in seconds.
  * @param {Function} onTimeUp - A callback function to be executed when the time reaches 0.
+ * @param {Boolean} stopTime - A boolean to stop the timer.
+ * 
  * @returns A timer that decrements the time every second.
  */
-const Timer = ({initialTime, onTimeUp}) => {
+const Timer = ({ initialTime, onTimeUp, stopTime }) => {
 
     const [timeLeft, setTimeLeft] = useState(initialTime);
     const className = timeLeft <= 5 ? "game-timer-warning-minus-5" : (timeLeft <= 10 ? "game-timer-warning-minus-10" : "game-timer");
@@ -20,12 +22,13 @@ const Timer = ({initialTime, onTimeUp}) => {
         }
 
         const timer = setInterval(() => {
-            setTimeLeft((prevTime) => prevTime - 1);
+            if (!stopTime)
+                setTimeLeft((prevTime) => prevTime - 1);
         }, 1000);
 
         return () => clearInterval(timer); // Clean up the timer when the component is unmounted
     }, [timeLeft, onTimeUp]);
-    
+
     return (
         <div className={className}>
             {timeLeft}s
