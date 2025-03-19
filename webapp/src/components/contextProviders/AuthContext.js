de191d3665335195f3037ade8f1067aa8365f549
javiersanabriamiranda <javier.sanabria.miranda@gmail.com>
2025-03-19 13:34:53 +0100

    Implementación proveedor del contexto de autenticación
    Se ha implementado un componente que proveerá a la aplicación del contexto de autenticación del usuario

---
+++ webapp/src/components/contextProviders/AuthContext.js
@@ -0,0 +1,61 @@
+import { createContext, useState, useEffect, Component } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+
+/**
+ * Component that provides the authentication context to its children. 
+ * It will bring to the children the user and token stored in the local and session storage and the login and logout functions.
+ * It keeps the user in the local storage to persist his information in the browser.
+ * The token is stored in the session storage to avoid persisting it in the browser (for more security).
+ * 
+ * @param {Component} children that will be wrapped by the provider and receive the context
+ * @returns component that provides the authentication context
+ */
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        const storedToken = sessionStorage.getItem("token");
+
+        if (storedUser && storedToken) {
+            setUser(JSON.parse(storedUser));
+            setToken(storedToken);
+        }
+    }, []);
+
+    const login = async (username, password) => {
+        try {
+            const res = await axios.post(apiEndpoint + "/login", {username, password});
+
+            const { token, id } = res.data;
+            setUser({ username, id });
+            setToken(token);
+            
+            localStorage.setItem("user", JSON.stringify({ username, id }));
+            sessionStorage.setItem("token", token);
+        } catch (error) {
+            console.error("Error en el login:", error.response?.data || error.message);
+        }
+    };
+
+
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("user");
+        sessionStorage.removeItem("token");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, token, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
