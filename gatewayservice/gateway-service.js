3edee8d6e5164009cacbf478251c9e39ad2357a3
Aitorsiius <aitorcius2004@gmail.com>
2025-03-27 16:56:45 +0100

    Cambio de LLM a Gemini

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -108,7 +108,7 @@ app.post('/askllm/clue', async (req, res) => {
   try {
     const { name, userQuestion, language } = req.body;
 
-    let model = "empathy";
+    let model = "gemini";
     let attempts = 0;
     let answer = "idk";
 
@@ -159,7 +159,7 @@ app.post('/askllm/welcome', async (req, res) => {
   try {
     const { name, language } = req.body;
 
-    let model = "empathy";
+    let model = "gemini";
     let question = "Saluda a " + name + " de forma educada y deséale suerte para su partida de WiChat. Sé conciso, UNA FRASE. Debes responder en " + getLanguage(language) + ".";
     let answer = await axios.post(llmServiceUrl+'/ask', { question, model });
 
