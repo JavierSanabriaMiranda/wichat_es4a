38e289002ada21f6c07f1a91f9e603f16d3c1ec3
javiersanabriamiranda <javier.sanabria.miranda@gmail.com>
2025-04-05 00:55:46 +0200

    Implementación tests Login y AddUser

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -6,3 +6,22 @@ import '@testing-library/jest-dom';
 //añadido ana
 import { TextEncoder } from 'util';
 global.TextEncoder = TextEncoder;
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from '../public/locales/en/translation.json';
+import translationES from '../public/locales/es/translation.json';
+
+i18n.use(initReactI18next).init({
+    lng: 'en', // o puedes leerlo de process.env para tests multiidioma
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: translationEN },
+      es: { translation: translationES },
+    },
+  });
