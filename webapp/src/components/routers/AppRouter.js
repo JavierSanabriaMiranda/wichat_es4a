1368aa7a5eea3b8f0fde432f8df8febf19a6954c
claudianistal <uo294420@uniovi.es>
2025-04-08 13:09:53 +0200

    Eliminación de json de prueba historial

--- webapp/src/components/routers/AppRouter.js
+++ webapp/src/components/routers/AppRouter.js
@@ -9,54 +9,6 @@ import { AuthRoute } from './AuthRoute';
 import { GameConfigProvider } from '../contextProviders/GameConfigProvider';
 
 
-const gameHistory = [
-  {
-    "points": 1450,
-    "correctAnswers": 18,
-    "totalQuestions": 30,
-    "date": "01/02/25",
-    "questions": [
-      {
-        "text": "¿Cuál es el cuadro más famoso de Leonardo Da Vinci?",
-        "imageUrl": "/logo512.png",
-        "wasUserCorrect": true,
-        "selectedAnswer": "Respuesta 2",
-        "answers": [
-          { "text": "Respuesta 1", "isCorrect": false },
-          { "text": "Respuesta 2", "isCorrect": true },
-          { "text": "Respuesta 3", "isCorrect": false },
-          { "text": "Respuesta 4", "isCorrect": false }
-        ]
-      },
-      {
-        "text": "¿Cuál es el lenguaje de programación más popular en 2025?",
-        "imageUrl": "/logo512.png",
-        "wasUserCorrect": false,
-        "selectedAnswer": "Respuesta D",
-        "answers": [
-          { "text": "Respuesta A", "isCorrect": false },
-          { "text": "Respuesta B", "isCorrect": false },
-          { "text": "Respuesta C", "isCorrect": true },
-          { "text": "Respuesta D", "isCorrect": false }
-        ]
-      },
-      {
-        "text": "¿En qué año se descubrió América?",
-        "imageUrl": "/logo512.png",
-        "wasUserCorrect": false,
-        "selectedAnswer": null,  // Pregunta saltada
-        "answers": [
-          { "text": "Opción 1", "isCorrect": false },
-          { "text": "Opción 2", "isCorrect": false },
-          { "text": "Opción 3", "isCorrect": true },
-          { "text": "Opción 4", "isCorrect": false }
-        ]
-      }
-    ]
-  }
-];
-
-
 const router = createBrowserRouter([
   {
     path: '/',
