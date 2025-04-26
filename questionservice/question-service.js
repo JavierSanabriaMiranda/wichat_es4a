2c952c599342dd493ae27eb58143d8ea663ddafa
uo287543 <uo287543@uniovi.es>
2025-04-26 23:50:59 +0200

    todook funciona

--- questionservice/question-service.js
+++ questionservice/question-service.js
@@ -73,7 +73,7 @@ function filterUnique(results, label, limit) {
  * 
  */
 async function executeQuery(query) {
-  const offset = 600;
+  const offset = 150;
   const finalQuery = query + ` LIMIT `+ offset +`OFFSET ${offset}`;
   const response = await axios.post(url, finalQuery, {
     headers: {
