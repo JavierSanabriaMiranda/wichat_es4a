45348a918a398e1d745944aced4377b7398523a9
Aitorsiius <aitorcius2004@gmail.com>
2025-05-03 00:10:22 +0200

    Fix tests de carga

--- testing/artillery/processor.js
+++ testing/artillery/processor.js
@@ -7,13 +7,8 @@ module.exports = {
 };
 
 function generateCredentials(context, events, next) {
-  // Usa el método correcto de Faker
-  const randomName = faker.internet.username();
-
-  // Añádele algo para garantizar unicidad (timestamp o UUID)
-  const uniqueSuffix = Date.now();              // o faker.datatype.uuid()
-  const uname = `${randomName}_${uniqueSuffix}`;
-  const pwd   = 'Test1234';
+  const uname = `User_${Date.now()}`.slice(0, 30);
+  const pwd = 'Test12345';
 
   // Guarda en context.vars para que Artillery lo inyecte luego en {{ username }} / {{ password }}
   context.vars.username = uname;
