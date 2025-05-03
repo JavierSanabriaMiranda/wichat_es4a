53bacb2c7676321816100a9144daf17ac06e8d93
uo287543 <uo287543@uniovi.es>
2025-05-03 13:43:59 +0200

    había un bug: al volver a iniciar la aplicación seleccionaba el idioma del último desplegable seleccionado en lugar del idioma del sistema

--- webapp/src/i18n/i18next.js
+++ webapp/src/i18n/i18next.js
@@ -4,21 +4,27 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-if(!i18n.isInitialized) {
-i18n
-  .use(Backend) // To load instructions from a backend server or a file
-  .use(LanguageDetector) // Detects the language of the web browser
-  .use(initReactI18next) // Initializes i18next for React
-  .init({
-    fallbackLng: 'es', // Default language
-    debug: true, // Show useful information for debugging
+if (!i18n.isInitialized) {
+  i18n
+    .use(Backend) // To load instructions from a backend server or a file
+    .use(LanguageDetector) // Detects the language of the web browser
+    .use(initReactI18next) // Initializes i18next for React
+    .init({
+      fallbackLng: 'es', // Default language
+      debug: true, // Show useful information for debugging
 
-    interpolation: {
-      escapeValue: false, 
-    },
+      detection: {
+        // Orden de detección: navegador primero para que coja siempre
+        // el valor del idioma del navegador que el último idioma seleccionado
+        // en la sección anterior
+        order: ['navigator', 'localStorage', 'sessionStorage', 'cookie', 'htmlTag'],
+        checkWhitelist: true,
+      },
 
-  });
+      interpolation: {
+        escapeValue: false,
+      },
+    });
 }
 
 export default i18n;
-
