961beb80e63a8fd1b89a4a83399b0bda29d65ef6
javiersanabriamiranda <javier.sanabria.miranda@gmail.com>
2025-05-04 16:58:39 +0200

    Documentación QuestionImage

--- webapp/src/components/game/QuestionImage.js
+++ webapp/src/components/game/QuestionImage.js
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
 
+/**
+ * This component displays an image that can be clicked to zoom in.
+ * 
+ * @param {String} src - The source URL of the image.
+ * @param {String} alt - The alternative text for the image.
+ * @param {String} className - Additional CSS classes to apply to the image.
+ * @returns Component that displays the image and handles zoom functionality.
+ */
 const QuestionImage = ({ src, alt, className }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const isPng = src.toLowerCase().endsWith('.png');
+    
+    // State to manage the zoomed image visibility
+    const [isOpen, setIsOpen] = useState(false);
+    // Check if the image is a PNG to apply specific styles
+    const isPng = src.toLowerCase().endsWith('.png');
 
-  return (
-    <>
-      <img
-        src={src}
-        alt={alt}
-        className={className ? className : ''}
-        onClick={() => setIsOpen(true)}
-        onContextMenu={(e) => e.preventDefault()}
-      />
+    return (
+        <>
+            <img
+                src={src}
+                alt={alt}
+                className={className ? className : ''}
+                onClick={() => setIsOpen(true)}
+                onContextMenu={(e) => e.preventDefault()}
+            />
 
-      {isOpen && (
-        <div
-          className="zoomed-image-div"
-          onClick={() => setIsOpen(false)}
-        >
-          <img
-            className={`zoomed-image ${isPng ? 'no-border' : ''}`}
-            src={src}
-            alt={alt}
-          />
-        </div>
-      )}
-    </>
-  );
+            {isOpen && (
+                <div
+                    className="zoomed-image-div"
+                    onClick={() => setIsOpen(false)}
+                >
+                    <img
+                        className={`zoomed-image ${isPng ? 'no-border' : ''}`}
+                        src={src}
+                        alt={alt}
+                    />
+                </div>
+            )}
+        </>
+    );
 }
 
 export default QuestionImage;
